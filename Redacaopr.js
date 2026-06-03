75969001d77ef7e30b4a029afdc5023196c0fe57
hoWo <Yyax13@proton.me>
2026-06-03 15:56:36 -0300

    Update Redacaopr.js

--- Redacaopr.js
+++ Redacaopr.js
@@ -25,6 +25,7 @@
             btn.style.top = '320px';
             btn.style.left = '10px';
             btn.style.zIndex = 10001;
+            btn.style.backgroundColor = "#2aa22a";
 
             btn.onclick = () => {
                 resolve(ta.value);
@@ -84,9 +85,69 @@
         const saveUrl = `https://redacao-api.pr.gov.br/api/v2/redacao/salvar/${uid}`;
         dlog("URL de salvamento:", saveUrl);
 
-        const title = await getTextFromTextarea("Insira o título da redação:");
-        const text = await getTextFromTextarea("Insira o texto (redação):");
+        const geminiKey = await getTextFromTextarea("Insira sua chave de api gemini:");
+        
+        let title;
+        let text;
+        if (!geminiKey) {
+            title = await getTextFromTextarea("Insira o título da redação:");
+            text = await getTextFromTextarea("Insira o texto (redação):");
+        } else {
+            const ppText = getBody?.proposta?.proposta;
+            const textoMotivacional = getBody?.proposta?.textoMotivacional?.map((texts) => texts?.texto);
+            const prompt = `Redija um texto com base em: ${ppText}
 
+            O texto não deve ser formatado (no máximo parágrafos com espaços) e não deve conter o titulo.
+            A resposta deve ser apenas a redação/texto. Sem conter titulo, tema e outros.
+            A resposta deve ter entre 200 e 290 palavras.
+
+            Os parágrafos não são linhas vazias e sim um tab (4 espaços), exemplo:
+
+            "
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer libero neque, gravida vel accumsan in, mattis posuere risus. Nam ornare fringilla elementum. 
+            Quisque rutrum diam nisl, a elementum ante molestie ut. Morbi eget nisi eu mi iaculis congue et vitae dui. Maecenas finibus turpis eleifend, vestibulum purus at, viverra est. Pellentesque eu placerat leo, eget maximus arcu. 
+            Aenean id purus at justo pulvinar volutpat vel vel nisi. Aliquam non lectus nisl. Nam lobortis ex ut mollis ornare. Sed dapibus viverra porta. In sagittis orci quis justo aliquet auctor. 
+            Etiam eu nisl ac enim laoreet facilisis. In turpis nibh, porta nec elit non, finibus ultricies mi.
+            "
+            
+            Textos de apoio:
+            
+            ${textoMotivacional.join("\n")}`;
+
+            dlog("Prompt gerado para Gemini:", prompt);
+            const geminiEndpoint = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent";
+
+            try {
+                const res = await fetch(geminiEndpoint, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "x-goog-api-key": geminiKey
+                    },
+                    body: JSON.stringify({
+                        contents: [
+                            {
+                                parts: [
+                                    { text: prompt }
+                                ]
+                            }
+                        ]
+                    })
+                });
+
+                const data = await res.json();
+                console.log("Resposta Gemini:", data);
+
+                // Pega o texto gerado
+                text = data?.candidates?.[0]?.content?.parts?.[0]?.text || "";
+                console.log("Texto gerado:", text);
+
+            } catch (err) {
+                console.error("[ERRO GEMINI]", err);
+            }
+
+        }
+        
         const payload = {
             essayId: essayId || null,
             text,
@@ -124,3 +185,4 @@
 
     window.location.reload();
 })();
+
